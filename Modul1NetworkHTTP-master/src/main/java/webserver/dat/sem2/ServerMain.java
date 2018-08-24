37188e4bd6bf018f85804befdcffcfd2931e2162
Michael <mich561d@hotmail.com>
2018-08-24 07:23:29 +0200

    Update

--- Modul1NetworkHTTP-master/src/main/java/webserver/dat/sem2/ServerMain.java
+++ Modul1NetworkHTTP-master/src/main/java/webserver/dat/sem2/ServerMain.java
@@ -50,6 +50,7 @@ public class ServerMain {
         } catch (Exception e) {
             server = new ServerSocket(0);
         }
+        //ServerSocket server = new ServerSocket(8080);
         System.out.println("Listening for connection on port " + server.getLocalPort() + "....");
         while (true) { // keep listening (as is normal for a server)
             try (Socket socket = server.accept()) {
